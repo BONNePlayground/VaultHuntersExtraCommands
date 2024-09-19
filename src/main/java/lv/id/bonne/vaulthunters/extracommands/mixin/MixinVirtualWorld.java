8bd7ce8610d2176fbd935e0aa5f45830de5f2852
BONNe <bonne@bonne.id.lv>
2024-09-19 09:50:52 +0300

    Replace with shadow.

--- src/main/java/lv/id/bonne/vaulthunters/extracommands/mixin/MixinVirtualWorld.java
+++ src/main/java/lv/id/bonne/vaulthunters/extracommands/mixin/MixinVirtualWorld.java
@@ -8,6 +8,7 @@ package lv.id.bonne.vaulthunters.extracommands.mixin;
 
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,20 +23,24 @@ import lv.id.bonne.vaulthunters.extracommands.data.ExtraCommandsWorldData;
 
 
 @Mixin(value = VirtualWorld.class)
-public class MixinVirtualWorld
+public abstract class MixinVirtualWorld
 {
+    @Shadow
+    public abstract boolean isMarkedForDeletion();
+
+
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void tickServerPause(BooleanSupplier hasTimeLeft, CallbackInfo ci)
     {
-        VirtualWorld world = ((VirtualWorld) (Object)this);
-
-        if (world.isMarkedForDeletion())
+        if (this.isMarkedForDeletion())
         {
             // Maybe processing clock while world is deleting causes ome issues?
             // It would be weird, but that is my only remaining code that could affect
             // getting into loop
             return;
         }
+
+        VirtualWorld world = ((VirtualWorld) (Object)this);
 
         ServerVaults.get(world).ifPresent(vault -> {
             vault.ifPresent(Vault.CLOCK, tickClock -> {
